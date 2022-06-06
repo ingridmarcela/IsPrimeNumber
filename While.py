9655d764a22e5eba3af7753267c4e05eda06adb6
Ingrid Marcela Hernandez Pineda <IHERNAND@proteccion.com.co>
2022-06-06 15:09:18 -0500

    first commit

---
+++ While.py
@@ -0,0 +1,17 @@
+# analiza si un número es o no primo
+# un número es primo si es divisible por el mismo y por el 1 unicamente. SI es divisible por mas numeros, ya no es primo
+valor=0
+numero=int(input("Enter your number\n"))
+a=int(numero)
+i=1
+while i <= a+1:
+ if(a % i==0):
+  #para saber cuántas veces ese número fue divisible
+  #por cada uno de los numeros que evalua  
+  valor=valor+1
+ i = i+1	
+# Si ese valor es diferente de dos es porque No es primo
+if(valor!=2):
+ print(str ("The number ") + str(a) + str(" is not a prime number"))
+else:
+ print(str ("The number ") + str(a) + str(" is a prime number"))
